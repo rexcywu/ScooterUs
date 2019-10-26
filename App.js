355757170eee5b716398f6e0aa065f9cc97a806f
TANG-Ning <anne510631@gmail.com>
2019-10-26 13:22:07 +0800

    nothing

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { ThemeProvider } from 'react-native-elements';
+import { ThemeProvider, SearchBar } from 'react-native-elements';
 import LogInScreen from './components/LogInScreen';
+
 
 export default function App() {
   return (
     <ThemeProvider>
+      <SearchBar/>
       <LogInScreen />
-    </ThemeProvider>
-  );
+      </ThemeProvider>
+      
+  )
 }
