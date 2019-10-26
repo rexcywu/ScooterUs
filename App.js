b4fae9f86cad7bfd0804dc8ff9bea27b23033315
Yu Chao <casd82@gmail.com>
2019-10-26 13:38:58 +0800

    Overall app flow.

--- App.js
+++ App.js
@@ -1,45 +1,48 @@
 import React, { useState } from 'react';
 import { ThemeProvider, Header } from 'react-native-elements';
 import LogInScreen from './components/LogInScreen';
-<<<<<<< HEAD
 import MainScreen from './components/MainScreen';
 import DetailsScreen from './components/DetailsScreen';
+import TrafficlightModal from './components/TrafficLightModal';
 
 const titles = {
   login: 'Log In',
   main: 'Information',
   details: 'Details'
 };
-=======
-import TrafficlightModal from './components/TrafficLightModal';
->>>>>>> 8c6d9987623ea4081426098ba95d7c8c97a2147d
 
 export default function App() {
   [currScreen, setCurrScreen] = useState('login');
+  [modalShow, setModalShow] = useState(false);
 
-  function loginButtonHandler() {
-    setCurrScreen('main');
+  function gotoScene(sceneName) {
+    setCurrScreen(sceneName);
+  }
+
+  function setTrafficLightHandler(show) {
+    setModalShow(show);
   }
 
   let screen;
   if (currScreen === 'login') {
-    screen = <LogInScreen loginClicked={loginButtonHandler} />;
+    screen = <LogInScreen loginClicked={() => gotoScene('main')} />;
   } else if (currScreen === 'main') {
-    screen = <MainScreen />;
+    screen = <MainScreen showModalClicked={() => setTrafficLightHandler(true)} />;
   } else if (currScreen === 'details') {
     screen = <DetailsScreen />;
   }
 
   return (
     <ThemeProvider>
-<<<<<<< HEAD
       <Header
         centerComponent={{ text: titles[currScreen], style: { color: '#ffffff', fontSize: 22 } }}
       />
       {screen}
-=======
-      <TrafficlightModal />
->>>>>>> 8c6d9987623ea4081426098ba95d7c8c97a2147d
+      <TrafficlightModal
+        visible={modalShow}
+        cancelClicked={() => setTrafficLightHandler(false)}
+        toDetails={() => { gotoScene('details'); setTrafficLightHandler(false) }}
+      />
     </ThemeProvider>
   );
 }
