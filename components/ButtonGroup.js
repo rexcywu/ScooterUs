dd75e460a95ccf986505a8400b29332422d6dc7d
Yu Chao <casd82@gmail.com>
2019-10-26 22:04:52 +0800

    Checked button.

--- components/ButtonGroup.js
+++ components/ButtonGroup.js
@@ -1,21 +1,21 @@
-import React, {Component} from 'react';
-import { ThemeProvider, ButtonGroup} from 'react-native-elements';
+import React from 'react';
+import { ThemeProvider, ButtonGroup } from 'react-native-elements';
 
 export default function buttonGroup() {
-   
-      
-    const buttons = ['Food', 'List', 'Pay!'];
-    const  selectedIndex = 0;
-      
-        return (
+
+
+  const buttons = ['Food', 'List', 'Pay!'];
+  const selectedIndex = 0;
+
+  return (
     <ThemeProvider>
       <ButtonGroup
-       underlayColor='#FFCCCC'
+        underlayColor='#FFCCCC'
         selectedIndex={selectedIndex}
         buttons={buttons}
-        containerStyle={{height: 50}}
+        containerStyle={{ height: 50 }}
       />
-      </ThemeProvider>
-    );
-    
-  }
+    </ThemeProvider>
+  );
+
+}
